52b5c73059689daedeafc0492b76f2f91a683658
Hanu <hanweonjin415@gmail.com>
2024-09-04 00:52:06 +0900

    basic glad/glfw setup

--- main.cc
+++ main.cc
@@ -1,6 +1,102 @@
+// System headers
 #include <iostream>
 
+// Vendor headers
+#include "glad/gl.h"
+#include "GLFW/glfw3.h"
+
+// OpenGL callback functions
+namespace gl {
+void APIENTRY DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                                   GLsizei length, const GLchar* message, const void* user_param);
+}
+
+// GLFW callback functions
+namespace glfw {
+void ErrorCallback(int error_code, const char* description);
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+}
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  // Set GLFW error callback
+  (void)glfwSetErrorCallback(glfw::ErrorCallback);
+
+  // Initialize GLFW
+  if (!glfwInit()) {
+    std::cerr << "glfwInit failed" << std::endl;
+    return 1;
+  }
+
+  // Configure window hints
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+  // Initialize window
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "glChess", nullptr, nullptr);
+
+  if (!window) {
+    std::cerr << "glfwCreateWindow failed" << std::endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // Set remaining GLFW callbacks
+  (void)glfwSetFramebufferSizeCallback(window, glfw::FramebufferSizeCallback);
+
+  if (!gladLoadGL(glfwGetProcAddress)) {
+    std::cerr << "gladLoadGL failed" << std::endl;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
+  // Enable and set OpenGL debug message callback
+  glEnable(GL_DEBUG_OUTPUT);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageCallback(gl::DebugMessageCallback, nullptr);
+
+  // Enable remaining OpenGL capabilities
+  glEnable(GL_DEPTH_TEST);
+
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+    // Handle events
+    glfwPollEvents();
+
+    // Update
+
+    // Render
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+  }
+
+  // Cleanup
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
+}
+
+// OpenGL callback functions
+namespace gl {
+void APIENTRY DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                                   GLsizei length, const GLchar* message, const void* user_param) {
+  std::cerr << "(" << id << ") " << message << std::endl;
+}
+}
+
+// GLFW callback functions
+namespace glfw {
+void ErrorCallback(int error_code, const char* description) {
+  std::cerr << "(" << error_code << ") " << description << std::endl;
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 }
